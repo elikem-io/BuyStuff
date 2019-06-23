20701fd9d7371dd8989fb76c86f05c75decfcf27
Precious Luke <lukeprecious@gmail.com>
2019-06-23 19:11:08 +0100

    header  and footers changes

--- header.php
+++ header.php
@@ -64,33 +64,15 @@
       <nav class="site-navigation text-right text-md-center" role="navigation">
         <div class="container">
           <ul class="site-menu js-clone-nav d-none d-md-block">
-            <li class="has-children active">
+            <li class="active">
               <a href="index.php">Home</a>
-              <ul class="dropdown">
-                <li><a href="#">Menu One</a></li>
-                <li><a href="#">Menu Two</a></li>
-                <li><a href="#">Menu Three</a></li>
-                <li class="has-children">
-                  <a href="#">Sub Menu</a>
-                  <ul class="dropdown">
-                    <li><a href="#">Menu One</a></li>
-                    <li><a href="#">Menu Two</a></li>
-                    <li><a href="#">Menu Three</a></li>
-                  </ul>
-                </li>
-              </ul>
+              
             </li>
-            <li class="has-children">
-              <a href="about.php">About</a>
-              <ul class="dropdown">
-                <li><a href="#">Menu One</a></li>
-                <li><a href="#">Menu Two</a></li>
-                <li><a href="#">Menu Three</a></li>
-              </ul>
-            </li>
+            <li><a href="shop.php">Account</a></li>
             <li><a href="shop.php">Shop</a></li>
             <li><a href="#">Catalogue</a></li>
             <li><a href="#">New Arrivals</a></li>
+            <li ><a href="about.php">About</a> </li>
             <li><a href="contact.php">Contact</a></li>
           </ul>
         </div>
